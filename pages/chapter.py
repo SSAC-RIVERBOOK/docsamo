72af3e0ad2e5d9935f85b2dd9648e4dd8939a303
root <root@DESKTOP-677MIJL>
2024-08-02 05:11:09 +0900

    fix/layout

--- pages/chapter.py
+++ pages/chapter.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils import show_menu, switch_page
+from utils import show_menu, show_user_data, show_user_status, switch_page
 
 st.session_state.game_page = "chapter"
 
@@ -24,4 +24,59 @@ with st.container():
     with col6:
         st.button("선녀와 나무꾼")
 
+st.markdown(
+    """
+    <style>
+    .block-container {
+        width: 100vw;
+        max-width: 816px;
+        height: 100vh;
+        max-height: 624px;
+        background-color: #f0f0f0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border-radius: 10px;
+    }
+    .st-emotion-cache-1jicfl2 {
+        padding-left: 0rem;
+        padding-right: 0rem;
+    }
+    .st-emotion-cache-1jicfl2 {
+        padding: 0rem 0rem 0rem;
+    }
+    .row-widget.stButton {
+        display: flex;
+        justify-content: center;
+    }
+    .st-emotion-cache-bm2z3a {
+        display: flex;
+        width: 100%;
+        overflow: auto;
+        -webkit-box-align: center;
+        align-items: center;
+        justify-content: center;
+        flex-direction: row;
+    }
+    .st-emotion-cache-1vt4y43 {
+        width: 150px;
+        height: 100px;
+        background-color: #D9D9D9;
+    }
+    .st-emotion-cache-ocqkz7 {
+        display: flex;
+        flex-wrap: wrap;
+        -webkit-box-flex: 1;
+        flex-grow: 1;
+        -webkit-box-align: stretch;
+        align-items: stretch;
+        gap: 0rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 show_menu(st.session_state.prev_page)
+show_user_data(["유형1", "유형2", "유형3"], [3, 7, 10], True, 400)
+show_user_status("이주배경어린이", "마지막 진행상황(챕터-라운드)", border=True)
