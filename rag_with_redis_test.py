5f6fbb696b5e44f13e69da805ea7f4a483d3a8ed
joonavel <joonavel@gmail.com>
2024-08-01 11:28:23 +0900

    ✨ Rag with Redis example
    Redis DB를 이용한 RAG 사용 예시입니다.

---
+++ rag_with_redis_test.py
@@ -0,0 +1,10 @@
+from langchain_community.vectorstores.redis import Redis
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+# rds = Redis.from_existing_index(embedding=embeddings, index_name="chapter1", schema=)
+rds = Redis(redis_url="redis://localhost:6379/0", index_name="chapter1", embedding=embeddings)
+retriever = rds.as_retriever()
+result = retriever.invoke("핵심 사건이 담겨있는 문서는?")
+print(result)
